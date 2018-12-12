66ed8ec7a59bc78ad6864dc3a2621770f56caadc
klar <a+git@pxd.ch>
2018-12-12 09:39:13 +0100

    Update weekly_vuls_mail.sh

--- check_vuls/weekly_vuls_mail.sh
+++ check_vuls/weekly_vuls_mail.sh
@@ -2,8 +2,9 @@
 
 #update cve database
 go-cve-dictionary fetchjvn -latest -dbpath=/var/vuls/cve/cve.sqlite3 > /var/vuls/weekly.txt
+go-cve-dictionary fetchnvd -latest -dbpath=/var/vuls/cve/cve.sqlite3 >> /var/vuls/weekly.txt
 
 #scan servers for current installed packages
-vuls scan -config=/var/vuls/config.toml -results-dir=/var/vuls/results -cachedb-path=/var/vuls/cache.db >> /var/vuls/weekly.txt
+vuls scan -config=/var/vuls/config.toml -results-dir=/var/vuls/results -cachedb-path=/var/vuls/cache/cache.db >> /var/vuls/weekly.txt
 
 cat /var/vuls/weekly.txt | mail -s "Weekly Vuls Report" <mytomail@address.com> -aFROM:<myfrommail@address.com>
