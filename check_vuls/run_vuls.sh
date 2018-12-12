29c16dc613ef883ad2f7246dea7fdb62858b9643
klar <a+git@pxd.ch>
2018-12-12 09:37:02 +0100

    Update run_vuls.sh

--- check_vuls/run_vuls.sh
+++ check_vuls/run_vuls.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 #update cve database
-go-cve-dictionary fetchjvn -latest -dbpath=/var/vuls/cve/cve.sqlite3 >/dev/null 2>&1 
+go-cve-dictionary fetchjvn -latest -dbpath=/var/vuls/cve/cve.sqlite3 >/dev/null 2>&1
+go-cve-dictionary fetchnvd -latest -dbpath=/var/vuls/cve/cve.sqlite3 >/dev/null 2>&1
 
 #scan servers for current installed packages
-vuls scan -config=/var/vuls/config.toml -results-dir=/var/vuls/results -cachedb-path=/var/vuls/cache.db >/dev/null 2>&1
+vuls scan -config=/var/vuls/config.toml -results-dir=/var/vuls/results -cachedb-path=/var/vuls/cache/cache.db >/dev/null 2>&1
+vuls report -config=/var/vuls/config.toml -results-dir=/var/vuls/results -cvedb-sqlite3-path /var/vuls/cve/cve.sqlite3 >/dev/null 2>&1
